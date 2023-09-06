c704b86678bda47612e672260ed11d50ad89375d
Nrindam <arindamnayak3@gmail.com>
2023-09-06 15:38:57 +0530

    header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -16,18 +16,18 @@
         <li class="nav-item">
           <a class="nav-link" href="#">Order</a>
         </li>
-
-        <li class="nav-item">
-          <a class="nav-link" href="#">add to cart</a>
-        </li>
-      </ul>
-      
-      
-     
-      <form class="d-flex" role="search">
+        <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
+      <li class="nav-item">
+          <a class="nav-link add" href="#">add to cart</a>
+        </li>
+      </ul>
+     
+      
+     
+     
     </div>
   </div>
 </nav>
